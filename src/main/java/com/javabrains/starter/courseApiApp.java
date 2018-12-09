a3b4f483ac18162ae8b91d6c69d542fa143452ce
saugaatallabadi <saugaatallabadi@gmail.com>
2018-12-09 09:37:40 +0400

    First commit

---
+++ src/main/java/com/javabrains/starter/courseApiApp.java
@@ -0,0 +1,13 @@
+package com.javabrains.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class courseApiApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(courseApiApp.class, args);
+	}
+	
+}
